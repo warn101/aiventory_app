dca86e33d9c6b9f8a597fa404b1257fb1fb5065a
warn101 <tahsinshikder202@gmail.com>
2025-06-28 19:31:08 +0600

    Fix AuthProvider and BookmarkProvider order

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import AuthLoader from './components/AuthLoader.tsx';
+import { AuthProvider } from './contexts/AuthContext';
 import { BookmarkProvider } from './contexts/BookmarkContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthLoader>
-      <BookmarkProvider>
-        <App />
-      </BookmarkProvider>
-    </AuthLoader>
+    <AuthProvider>
+      <AuthLoader>
+        <BookmarkProvider>
+          <App />
+        </BookmarkProvider>
+      </AuthLoader>
+    </AuthProvider>
   </StrictMode>
 );
